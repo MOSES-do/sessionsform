6b1362f744d192d0454b9b0d224346f45806e99c
MOSES-do <68383557+MOSES-do@users.noreply.github.com>
2021-03-09 00:53:26 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php  
 
 session_start();
-if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+//if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
 
     header("location: home.php");
-}
+//}
 
 ?>
 <!DOCTYPE html>
